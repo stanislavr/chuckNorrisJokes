3cc1bd224ec82fba2ec040b4a076264f35ad219d
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-10-01 21:52:33 -0400

    added some input validation and alert msgs

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+UIalertMsg = document.querySelector("#alertMsg");
 document.querySelector(".get-jokes").addEventListener("click", getJokes);
 
 function getJokes(e) {
@@ -5,6 +6,11 @@ function getJokes(e) {
 
   // get a number of random jokes
   const number = document.querySelector('input[type="number"]').value;
+  // validate input
+  if (number.length == 0) {
+    showAlert("Please enter a number of jokes to be requested!", "red", 5000);
+    return;
+  }
 
   // making request
   const xhr = new XMLHttpRequest();
@@ -21,12 +27,25 @@ function getJokes(e) {
           output += `<li>${joke.joke}</li>`;
         });
       } else {
-        output += "<li>Something went wrong during the request</li>";
+        showAlert("Something went wrong during the request!", "red", 5000);
+        return;
       }
 
+      showAlert("Success!", "green", 3000);
       document.querySelector(".jokes").innerHTML = output;
     }
   };
 
   xhr.send();
+}
+
+// Show Alert Message
+function showAlert(msg, color, timeout) {
+  UIalertMsg.style.color = color;
+  UIalertMsg.textContent = msg;
+
+  // clear msg after timeout milliSec
+  setTimeout(function() {
+    UIalertMsg.textContent = "";
+  }, timeout);
 }
